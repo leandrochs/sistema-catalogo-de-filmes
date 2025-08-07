9d266cfbe38c43ef6c33369d69d1b639efd64fa8
leandrochs <leandrochagask@gmail.com>
2025-08-07 11:35:11 -0300

    fix: inserindo static nos metodos

--- src/Repository/FilmRepository.java
+++ src/Repository/FilmRepository.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class FilmRepository {
     private List<Film> films = new ArrayList<Film>();
 
-    public void saveFilm(Film film) {
+    public static void saveFilm(Film film) {
         films.add(film);
     }
 
-    public List<Film> readAllFilms() {
+    public static List<Film> readAllFilms() {
         return films;
     }
 
-    public List<Film> readFilmByName(String filmName) {
+    public static List<Film> readFilmByName(String filmName) {
         List<Film> filmsByName = films.stream()
                 .filter(film -> film.getName().equals(filmName))
                 .toList();
