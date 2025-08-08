ec97465adef9ac2b64a15aa0d6806ce184432c6f
leandrochs <leandrochagask@gmail.com>
2025-08-08 11:44:12 -0300

    fix: corrige nome de atributo

--- src/Model/Actor.java
+++ src/Model/Actor.java
@@ -1,25 +1,25 @@
 package Model;
 
 public class Actor {
-    private final String nome;
+    private final String name;
     private final String birthday;
     private final String nationality;
 
 
-    public Actor(String nome, String birthday, String nationality) {
-        this.nome = nome;
+    public Actor(String name, String birthday, String nationality) {
+        this.name = name;
         this.birthday = birthday;
         this.nationality = nationality;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
     @Override
     public String toString() {
         return "Actor{" +
-                "nome='" + nome + '\'' +
+                "nome='" + name + '\'' +
                 ", birthday='" + birthday + '\'' +
                 ", nationality='" + nationality + '\'' +
                 '}';
