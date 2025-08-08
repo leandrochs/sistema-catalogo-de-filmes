51605f6c8a15929fb8e0115b69d5a1eababa5fa3
leandrochs <leandrochagask@gmail.com>
2025-08-08 11:29:00 -0300

    feat: adiciona classe Actor

--- src/Model/Actor.java
+++ src/Model/Actor.java
@@ -2,12 +2,26 @@ package Model;
 
 public class Actor {
     private final String nome;
+    private final String birthday;
+    private final String nationality;
 
-    public Actor(String nome) {
+
+    public Actor(String nome, String birthday, String nationality) {
         this.nome = nome;
+        this.birthday = birthday;
+        this.nationality = nationality;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "nome='" + nome + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", nationality='" + nationality + '\'' +
+                '}';
     }
 }
