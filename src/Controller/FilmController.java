373daa185df54810ec88514939154fc468763496
leandrochs <leandrochagask@gmail.com>
2025-08-08 08:33:03 -0300

    feat: adiciona classe FilmController

--- src/Controller/FilmController.java
+++ src/Controller/FilmController.java
@@ -1,7 +1,28 @@
 package Controller;
 
+import Model.Film;
+import Repository.FilmRepository;
 
 public class FilmController {
+    private static FilmRepository repository;
 
+    public FilmController(FilmRepository repository) {
+        this.repository = repository;
+    }
 
+    public Film findFilmByName(String name) {
+        return (Film) repository.findByName(name);
+    }
+
+    public Film createAndSaveFilm(String title, String year, String budget, String synopsis, String gender, String time) {
+        Film film = new Film(title, year, budget, synopsis, gender, time);
+        repository.saveFilm(film);
+        return film;
+    }
 }
+
+
+/*
+(1) cadastrar filmes
+(6) pesquisar filme
+*/
